b543469df6b2ea22f33ed264671d778dad9b7e7a
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-07-22 20:00:31 -0500

    getting-started

---
+++ app/config.py
@@ -0,0 +1,21 @@
+"""app/config"""
+import logging
+import os
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+log = logging.getLogger(__name__)
+
+
+class Settings(BaseSettings):
+    """Settings"""
+    environment: str = os.getenv("ENVIRONMENT", "dev")
+    testing: bool = os.getenv("TESTING", 0)
+
+
+@lru_cache()
+def get_settings() -> BaseSettings:
+    """get settings"""
+    log.info("loading config settings from the environment")
+    return Settings()
---
+++ app/main.py
@@ -0,0 +1,16 @@
+"""app/main.py"""
+from fastapi import FastAPI, Depends
+
+from app.config import get_settings, Settings
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    """ping"""
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing,
+    }
